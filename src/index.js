f0ecda46e31696fa5d84aba99b34bd846d7ca6f2
Patrícia Moutinho <patdesigns@gmail.com>
2026-05-07 18:28:40 +0100

    update

--- src/index.js
+++ src/index.js
@@ -53,27 +53,28 @@ function handleSearchSubmit(event) {
 }
 
 function displayForecast() {
-  
-let days = ["Tue ", "Wed", "Thu", "Fri", "Sat"];
-let forecast ="";
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
 
-days.forEach(function(day){
-  forecastHtml = forecastHtml + 
- `
-<div class="weather-forecast-day">
-            <div class="weather-forecast-date">${day}</div>
-            <div class="weather-forecast-icon">🌥️</div>
-            <div class="weather-forecast-temperatures">
-              <div class="weather-forecast-temperature">
-                <strong> 15º </strong>
-              </div>
-              <div class="weather-forecast-temperature">9º</div> 
-              </div>
-          </div>`;
-});
-  
-let forecast = document.querySelector("#forecast");
-forecastElement.innerHTML=forecastHtml; 
+  days.forEach (function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>15º</strong>
+          </div>
+          <div class="weather-forecast-temperature">9º</div>
+        </div>
+      </div>
+    `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
 }
 
 let searchFormElement = document.querySelector("#search-form");
